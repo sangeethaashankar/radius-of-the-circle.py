b9df5bfc6968fb79044705f7442ee894866aafd3
sangeethaashankar <84265905+sangeethaashankar@users.noreply.github.com>
2021-05-16 15:28:09 +0530

    Add files via upload

---
+++ python extension.py
@@ -0,0 +1,7 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============== RESTART: C:/Users/User/Desktop/python extension.py ==============
+input the file name:abc.py
+extension of the file is:'py'
+>>> 
